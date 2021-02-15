1d5acaad522292c939262399afe54f70843bec08
eldercs <deny_1999@ukr.net>
2021-02-16 00:17:16 +0300

    add image

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -36,6 +36,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Snake.Properties.Resources.gelkirp21;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Name = "Form1";
             this.Text = "Form1";
